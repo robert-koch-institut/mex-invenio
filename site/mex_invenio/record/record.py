dd358c0ad13f9fcf3a3ac2dd2ff5259e085ce878
Hrafn Malmquist <hrafn.malmquist@gmail.com>
2025-07-10 12:42:24 +0100

    Hotfix/versioned redirect (#116)
    This PR fixes #114 by adding a version parameter to the redirect to mex
    identifier record landing page.
    
    The view redirect method is refactored and simplified.

--- site/mex_invenio/record/record.py
+++ site/mex_invenio/record/record.py
@@ -1,8 +1,9 @@
-from flask import render_template, abort
+from flask import render_template, abort, request, g
 from flask.views import MethodView
+
+from invenio_rdm_records.records.api import RDMRecord
 from invenio_rdm_records.resources.serializers import UIJSONSerializer
 from invenio_rdm_records.proxies import current_rdm_records_service
-from invenio_access.permissions import system_identity
 from invenio_pidstore.errors import PIDDoesNotExistError
 
 import json
@@ -15,20 +16,44 @@ class MexRecord(MethodView):
         self.template = "invenio_app_rdm/records/detail.html"
 
     def get(self, mex_id):
-        try:
-            record = _get_record_by_mex_id(mex_id)
-        except PIDDoesNotExistError:
-            abort(404)
-        except Exception:
-            abort(500)
+        version_id = request.args.get("version_id", None)
 
-        pid = record["id"]
-        service = current_rdm_records_service
-        record_item = service.read(system_identity, pid)
+        # Establish the version_id as an integer if it is provided
+        if version_id:
+            try:
+                version_id = int(version_id)
+            except ValueError:
+                version_id = None
+
+        if version_id:
+            # Try to fetch the record by mex_id and version_id
+            # Note: RDMRecord.model_cls is used to access the underlying SQLAlchemy model,
+            # this is because RDMRecord api methods expect a PID and not a mex_id.
+            record = RDMRecord.model_cls.query.filter(
+                RDMRecord.model_cls.json["custom_fields"]["mex:identifier"].as_string()
+                == mex_id,
+                RDMRecord.model_cls.index == version_id,
+            ).one_or_none()
+
+            if not record:
+                abort(404)
+            pid = record.json["id"]
+        else:
+            # Version_id is not provided, fetch the latest record by mex_id
+            try:
+                record = _get_record_by_mex_id(mex_id)
+            except PIDDoesNotExistError:
+                abort(404)
+            except Exception:
+                abort(500)
+
+            pid = record["id"]
+
+        record_item = current_rdm_records_service.read(g.identity, pid)
         ui_serializer = UIJSONSerializer()
         record_ui = ui_serializer.serialize_object(record_item.data)
 
-        linked_records_data = _get_linked_records_data(record, mex_id)
+        linked_records_data = _get_linked_records_data(record_item, mex_id)
 
         return render_template(
             self.template,
--- site/mex_invenio/views.py
+++ site/mex_invenio/views.py
@@ -1,13 +1,12 @@
-from flask import Blueprint, redirect, url_for, current_app, abort
+from flask import Blueprint, redirect, url_for, current_app, abort, g
+
 from .record.record import MexRecord
 
-from invenio_pidstore.resolver import Resolver
 from invenio_pidstore.errors import (
     PIDDoesNotExistError,
-    PIDMissingObjectError,
     PIDUnregistered,
 )
-from invenio_rdm_records.records.api import RDMRecord
+from invenio_rdm_records.proxies import current_rdm_records_service
 
 
 #
@@ -39,27 +38,34 @@ def create_blueprint(app):
 
 
 def redirect_to_mex(record_id):
+    """
+    Redirects to the MEX view based on the record ID,
+    also passes the version id if it is not the latest record.
+    :param record_id:
+    :return:
+    """
     record = None
-    resolver = Resolver(
-        pid_type="recid", object_type="rec", getter=RDMRecord.get_record
-    )
 
     try:
-        pid, record = resolver.resolve(record_id)
+        record = current_rdm_records_service.read(g.identity, record_id)
     except (PIDDoesNotExistError, PIDUnregistered):
         abort(404)
-    except PIDMissingObjectError as e:
-        current_app.logger.exception(
-            "No object assigned to {0}.".format(e.pid), extra={"pid": e.pid}
-        )
-        abort(500)
     except Exception as e:
         current_app.logger.exception("Unknown error occurred.", extra={"error": e})
         abort(500)
+
     try:
-        mex_id = record["custom_fields"]["mex:identifier"]
+        mex_id = record.data["custom_fields"]["mex:identifier"]
     except Exception as e:
         current_app.logger.exception("No mex id for the record {0}.".format(e))
         abort(500)
+
+    if not record.data["versions"]["is_latest"]:
+        # If the record is not the latest version, include version id
+        return redirect(
+            url_for(
+                ".mex_view", mex_id=mex_id, version_id=record.data["versions"]["index"]
+            )
+        )
 
     return redirect(url_for(".mex_view", mex_id=mex_id))
