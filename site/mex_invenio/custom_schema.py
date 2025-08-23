c6dc1d36e5a00ca9b06b1af1931f9c89041a0dfd
Hrafn Malmquist <hrafn.malmquist@gmail.com>
2025-08-23 23:28:08 +0100

    Functioning version

--- site/mex_invenio/custom_schema.py
+++ site/mex_invenio/custom_schema.py
@@ -20,5 +20,22 @@ class IndexDataSchema(Schema):
 
 class MexRDMRecordSchema(RDMRecordSchema):
     """MEX RDM record schema with custom index_data field."""
-    
+
     index_data = NestedAttribute(IndexDataSchema)
+    
+    # Let's try a simple raw field first to test if our schema is being used
+    '''test_field = fields.Raw(dump_only=True, default="MEX_SCHEMA_WORKING")
+    
+    index_data = fields.Method("get_index_data", dump_only=True)
+    
+    def get_index_data(self, obj):
+        """Get the index data for the record."""
+        # Check if obj already has index_data (from search dumper)
+        print(obj)
+        print('--- get_index_data called ---')
+        print(obj['index_data'])
+        if isinstance(obj, dict) and 'index_data' in obj and obj['index_data']:
+            return obj['index_data']
+        
+        # For record objects that don't have index_data yet
+        return {'notnoworking': True}'''
