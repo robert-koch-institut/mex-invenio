66363967544d23c613128232d997de7cb4d8fa74
Hrafn Malmquist <hrafn.malmquist@gmail.com>
2025-08-24 00:14:37 +0100

    Remove JSON schema

--- site/mex_invenio/records/jsonschemas/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright (C) 2025 MEX Invenio.
-#
-# MEX Invenio is free software; you can redistribute it and/or modify it
-# under the terms of the MIT License; see LICENSE file for more details.
-
-"""JSON schemas for MEX records."""
