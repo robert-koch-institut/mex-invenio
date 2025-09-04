b2dbe927bf846204050936d865878c1560008fb2
Aga <aga@cottagelabs.com>
2025-09-04 12:58:49 +0100

    remove header

--- site/mex_invenio/assets/semantic-ui/js/mex_invenio/SupportRecordData.js
+++ site/mex_invenio/assets/semantic-ui/js/mex_invenio/SupportRecordData.js
@@ -5,7 +5,6 @@ export const SupportRecordData = () => {
 
   return (
     <>
-      <h1> This is my component </h1>
       <p><strong>url: </strong> /records/mex/dO8iP5KEwJGZPXwCXKnFOZ/json </p>
       <Record mex_id="dO8iP5KEwJGZPXwCXKnFOZ" />
     </>
