b1eca19d5a3da2d1a7837e7af9328e43b7923118
Aga <aga@cottagelabs.com>
2025-11-17 13:08:38 +0000

    fix typos, reverse dash change

--- site/mex_invenio/assets/semantic-ui/js/mex_invenio/i18n.js
+++ site/mex_invenio/assets/semantic-ui/js/mex_invenio/i18n.js
@@ -2,7 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-import deTranslations from "./locales/de/translation.json";
+import deTranslations from "./locales/de/translations.json";
 
 const resources = {
     de: { translation: deTranslations },
