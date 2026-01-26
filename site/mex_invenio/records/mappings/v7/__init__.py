75006304fa0a88901e8164921af0aa2ab02953b0
Hrafn Malmquist <hrafn.malmquist@gmail.com>
2026-01-26 14:19:28 +0000

    Delete ES7 mappings, they won't be used

--- site/mex_invenio/records/mappings/v7/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright (C) 2024 MEX.
-#
-# MEX-Invenio is free software; you can redistribute it and/or modify
-# it under the terms of the MIT License; see LICENSE file for more details.
-
-"""MEX Records search mappings for Elasticsearch v7."""
